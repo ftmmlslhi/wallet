c7ce2cc405e1b9cca2aba2d6e665f4003266f6cd
fatemeh <molasalehi.fatemeh@gmail.com>
2024-05-14 16:43:34 +0330

    role check for taransaction

--- src/transaction/transaction.controller.ts
+++ src/transaction/transaction.controller.ts
@@ -1,21 +1,33 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Put } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Put, UseGuards } from '@nestjs/common';
 import { TransactionService } from './transaction.service';
 import { CreateTransactionDto } from './dto/create-transaction.dto';
 import { UpdateTransactionDto } from './dto/update-transaction.dto';
 import { Prisma } from '@prisma/client';
+import { AuthGuard } from 'src/auth/auth.guard';
 
+@UseGuards(AuthGuard)
 @Controller('transaction')
 export class TransactionController {
   constructor(private readonly transactionService: TransactionService) {}
 
   @Post()
-  create(@Body() transactionCreateInput : Prisma.transactionCreateInput,@Body() dto : CreateTransactionDto) {    
-    return this.transactionService.create(transactionCreateInput,dto);
+  create(@Body() transactionCreateInput : Prisma.transactionCreateInput, @Body() dto : CreateTransactionDto,  @Body() body: any) {    
+    let userRole = body.Payload.role;
+    if (userRole === 'admin'|| 'user') {
+      return this.transactionService.create(transactionCreateInput,dto);
+    } else {
+      return 'access denied!';
+    }
   }
 
   @Put('/admin/:id')
-  update(@Param('id') id: string, @Body() updateTransactionDto: Prisma.transactionUpdateInput) {
-    return this.transactionService.update(+id, updateTransactionDto);
+  update(@Param('id') id: string, @Body() updateTransactionDto: Prisma.transactionUpdateInput,  @Body() body: any) {
+    let userRole = body.Payload.role;
+    if (userRole === 'admin') {
+      return this.transactionService.update(+id, updateTransactionDto);
+    } else {
+      return 'access denied!';
+    }
   }
 
 }
